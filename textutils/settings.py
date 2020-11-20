a904f2995770fbc1c87c388d31851d4c8a00cd13
Shivansh Singh <shivanssingh2007@gmail.com>
2020-11-20 11:32:29 +0530

    edit

--- textutils/settings.py
+++ textutils/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j$#z5o$!(fe9l)o@kb1urxg#dkyvu6#!02^9tl&&x2zssy+goy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
